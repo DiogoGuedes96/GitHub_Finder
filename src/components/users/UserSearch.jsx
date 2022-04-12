8ff538626bd01bd86a06d55d287bfea2358bddd8
Diogo Guedest <diogo.guedes@integer.pt>
2022-04-12 15:22:08 +0100

    Clear Users

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,41 +1,42 @@
-import { useState, useContext } from 'react'
-import GithubContext from '../../context/github/GithubContext'
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 function UserSearch() {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
-  const { users, searchUsers } = useContext(GithubContext)
+  const { users, searchUsers, clearUsers } = useContext(GithubContext);
+  const { setAlert } = useContext(AlertContext);
 
-  const handleChange = (e) => setText(e.target.value)
+  const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if (text === '') {
-      alert('Please enter something')
+    if (text === "") {
+      setAlert("Please enter something", "error");
     } else {
-      searchUsers(text)
-
-      setText('')
+      searchUsers(text);
+      setText("");
     }
-  }
-
+  };
+  
   return (
-    <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
         <form onSubmit={handleSubmit}>
-          <div className='form-control'>
-            <div className='relative'>
+          <div className="form-control">
+            <div className="relative">
               <input
-                type='text'
-                className='w-full pr-40 bg-gray-200 input input-lg text-black'
-                placeholder='Search'
+                type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search"
                 value={text}
                 onChange={handleChange}
               />
               <button
-                type='submit'
-                className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+                type="submit"
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
               >
                 Go
               </button>
@@ -45,11 +46,12 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button className='btn btn-ghost btn-lg'>Clear</button>
+          <button onClick={clearUsers} className="btn btn-ghost btn-lg">
+            Clear
+          </button>
         </div>
       )}
     </div>
-  )
+  );
 }
-
-export default UserSearch
+export default UserSearch;
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,42 +1,33 @@
 import { createContext, useReducer } from 'react'
 import githubReducer from './GithubReducer'
-
 const GithubContext = createContext()
-
-const GITHUB_URL   = process.env.REACT_APP_GITHUB_URL
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     loading: false,
   }
-
   const [state, dispatch] = useReducer(githubReducer, initialState)
-
   // Get search results
   const searchUsers = async (text) => {
     setLoading()
-
     const params = new URLSearchParams({
       q: text,
     })
-    console.log(GITHUB_URL);
     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
-
-    
-
     const { items } = await response.json()
-
     dispatch({
       type: 'GET_USERS',
       payload: items,
     })
   }
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
@@ -47,11 +38,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers,
       }}
     >
       {children}
     </GithubContext.Provider>
   )
 }
-
 export default GithubContext
